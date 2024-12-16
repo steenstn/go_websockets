e690cd5e868c33a800bf0afd6ab6406f6913d93b
Alexander Steen <alexander.steen@qapital.com>
2024-12-16 21:03:44 +0100

    Snakes

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
-	"math"
 	"math/rand"
 	"net/http"
 	"time"
@@ -15,12 +14,43 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+type Direction int
+
+const (
+	up    Direction = 0
+	left            = 1
+	down            = 2
+	right           = 3
+)
+
 type Client struct {
-	position   Movable
-	vx         float64
-	vy         float64
+	x          int
+	y          int
+	direction  Direction
+	snake      []TailSegment
 	connection *websocket.Conn
 	alive      bool
+	tailLength int
+}
+
+type Pickup struct {
+	x          int
+	y          int
+	pickupType int
+}
+
+type TailSegment struct {
+	x int
+	y int
+}
+
+func toTailPosition(tailSegment []TailSegment, tailLength int) []TailPosition {
+	result := make([]TailPosition, tailLength)
+	for i := 0; i < tailLength; i++ {
+		result[i].X = tailSegment[i].x
+		result[i].Y = tailSegment[i].y
+	}
+	return result
 }
 
 type Asteroid struct {
@@ -44,31 +74,30 @@ type Bullet struct {
 type PlayerPosition struct {
 	X         int
 	Y         int
-	Direction float64
+	Direction Direction
+	Tail      []TailPosition
 }
 
-type AsteroidPosition struct {
-	X    int
-	Y    int
-	Size int
-}
-type BulletPosition struct {
+type TailPosition struct {
 	X int
 	Y int
 }
 
-type GameState struct {
-	Players   []PlayerPosition
-	Asteroids []AsteroidPosition
-	Bullets   []BulletPosition
+type PickupPosition struct {
+	X    int
+	Y    int
+	Type int
 }
 
-const levelWidth = 640
-const levelHeight = 480
+type GameState struct {
+	Players []PlayerPosition
+}
 
-var clients = make([]Client, 0)
-var asteroids = make([]Asteroid, 0)
-var bullets = make([]Bullet, 0)
+const levelWidth = 100
+const levelHeight = 100
+
+var clients = make([]*Client, 0)
+var pickups = make([]Pickup, 3)
 
 var gameRunning = false
 
@@ -82,15 +111,20 @@ func main() {
 
 func game(responseWriter http.ResponseWriter, request *http.Request) {
 	conn, _ := upgrader.Upgrade(responseWriter, request, nil)
-	client := Client{Movable{
-		x:         float64(50.0 + 50*len(clients)),
-		y:         50.0,
-		direction: 0,
-		speed:     0,
-	}, 0.0, 0.0, conn, true}
-	clients = append(clients, client)
+	client := Client{
+		x:          10 + 15*len(clients),
+		y:          5,
+		direction:  down,
+		connection: conn,
+		alive:      true,
+		snake:      make([]TailSegment, 100),
+		tailLength: 5,
+	}
+	client.snake[0].x = 10 + 10*len(clients)
+	client.snake[0].y = 10
+	clients = append(clients, &client)
 
-	go inputLoop(len(clients) - 1)
+	go inputLoop(&client)
 	if gameRunning || len(clients) < 2 {
 		return
 	}
@@ -103,21 +137,17 @@ func game(responseWriter http.ResponseWriter, request *http.Request) {
 
 func initGame() {
 	println("Init game")
-	for i := 0; i < 5; i++ {
-		asteroids = append(asteroids, Asteroid{position: Movable{
-			x:         float64(100 + 100*i),
-			y:         200.0 + rand.Float64()*150,
-			direction: rand.Float64() * 2 * math.Pi,
-			speed:     rand.Float64() * 2},
-			size: int(rand.Float64()*5 + 1),
-		})
+
+	for i := 0; i < len(pickups); i++ {
+		pickups[i].pickupType = 0
+		pickups[i].x = rand.Intn(2 + levelWidth - 4)
+		pickups[i].y = rand.Intn(2 + levelHeight - 4)
 	}
 }
 
-func inputLoop(index int) {
+func inputLoop(c *Client) {
 	println("Starting input loop")
 	for {
-		c := &clients[index] // Why can't this be outside the loop?
 		if c.alive == false {
 			break
 		}
@@ -128,21 +158,21 @@ func inputLoop(index int) {
 		}
 		fmt.Printf("%s sent: %s\n", c.connection.RemoteAddr(), string(msg))
 		var input = string(msg)
-		if input == "up" {
-			c.position.speed = 0.2
-		} else if input == "left" {
-			c.position.direction -= 0.15
-		} else if input == "down" {
-			c.position.speed = 0
-		} else if input == "right" {
-			c.position.direction += 0.15
+		if input == "up" && c.direction != down {
+			c.direction = up
+		} else if input == "left" && c.direction != right {
+			c.direction = left
+		} else if input == "down" && c.direction != up {
+			c.direction = down
+		} else if input == "right" && c.direction != left {
+			c.direction = right
 		} else if input == "space" {
-			bullets = append(bullets, Bullet{Movable{
+			/*bullets = append(bullets, Bullet{Movable{
 				x:         c.position.x,
 				y:         c.position.y,
 				direction: c.position.direction,
 				speed:     math.Sqrt(c.vx*c.vx+c.vy*c.vy) + 1.0,
-			}, true})
+			}, true})*/
 		}
 	}
 }
@@ -155,90 +185,77 @@ func gameLoop() {
 				continue
 			}
 
-			clients[i].vx += clients[i].position.speed * math.Cos(clients[i].position.direction)
-			clients[i].vy += clients[i].position.speed * math.Sin(clients[i].position.direction)
-			clients[i].position.x += clients[i].vx
-			clients[i].position.y += clients[i].vy
-			wrapAround(&clients[i].position, levelWidth, levelHeight, 10)
+			moveSnake(&clients[i].snake, clients[i].tailLength, clients[i].direction)
+			checkCollision(clients[i])
 
-			clientPositions = append(clientPositions, PlayerPosition{int(math.Round(clients[i].position.x)), int(math.Round(clients[i].position.y)), clients[i].position.direction})
-		}
+			/*	clients[i].vy += clients[i].position.speed * math.Sin(clients[i].position.direction)
+				clients[i].position.x += clients[i].vx
+				clients[i].position.y += clients[i].vy*/
+			wrapAround(clients[i], levelWidth, levelHeight, 1)
 
-		asteroidPositions := make([]AsteroidPosition, 0)
-		for i := 0; i < len(asteroids); i++ {
-			if asteroids[i].size <= 0 {
-				continue
-			}
-			asteroids[i].position.x += asteroids[i].position.speed * math.Cos(asteroids[i].position.direction)
-			asteroids[i].position.y += asteroids[i].position.speed * math.Sin(asteroids[i].position.direction)
-
-			wrapAround(&asteroids[i].position, levelWidth, levelHeight, 50)
-			for j := 0; j < len(clients); j++ {
-				if overlap(asteroids[i].position.x, asteroids[i].position.y, float64(asteroids[i].size*9.0), float64(asteroids[i].size*9.0), clients[j].position.x, clients[j].position.y, 10, 10) {
-					clients[j].alive = false
-				}
-			}
-
-			asteroidPositions = append(asteroidPositions, AsteroidPosition{
-				X:    int(math.Round(asteroids[i].position.x)),
-				Y:    int(math.Round(asteroids[i].position.y)),
-				Size: asteroids[i].size,
-			})
-		}
-
-		bulletPositions := make([]BulletPosition, 0)
-		for i := 0; i < len(bullets); i++ {
-			if !bullets[i].alive {
-				continue
-			}
-			currentBullet := &bullets[i].position
-			currentBullet.x += currentBullet.speed * math.Cos(currentBullet.direction)
-			currentBullet.y += currentBullet.speed * math.Sin(currentBullet.direction)
-
-			for j := 0; j < len(asteroids); j++ {
-				if asteroids[j].size > 0 && overlap(currentBullet.x, currentBullet.y, 6, 6, asteroids[j].position.x, asteroids[j].position.y, float64(asteroids[j].size*10.0), float64(asteroids[j].size*10.0)) {
-					asteroids[j].size--
-					bullets[i].alive = false
-					if asteroids[j].size > 0 {
-						asteroids[j].position.direction = rand.Float64() * 2 * math.Pi
-						asteroids = append(asteroids, Asteroid{
-							position: Movable{asteroids[j].position.x, asteroids[j].position.y, rand.Float64() * 2 * math.Pi, asteroids[j].position.speed * 1.2},
-							size:     asteroids[j].size,
-							alive:    true,
-						})
-					}
-				}
-			}
-
-			bulletPositions = append(bulletPositions, BulletPosition{
-				X: int(currentBullet.x),
-				Y: int(currentBullet.y),
-			})
+			clientPositions = append(clientPositions, PlayerPosition{clients[i].snake[0].x, clients[i].snake[0].y, clients[i].direction, toTailPosition(clients[i].snake, clients[i].tailLength)})
 		}
 
 		gameState := GameState{
-			Players:   clientPositions,
-			Asteroids: asteroidPositions,
-			Bullets:   bulletPositions,
+			Players: clientPositions,
 		}
 
 		broadcastGameState(gameState)
-		time.Sleep(30 * time.Millisecond)
+		time.Sleep(80 * time.Millisecond)
 	}
 }
 
-func wrapAround(position *Movable, xMax float64, yMax float64, buffer float64) {
-	if position.x > xMax+buffer {
-		position.x = -buffer
+func moveSnake(snakePointer *[]TailSegment, tailLength int, direction Direction) {
+	var snake = *snakePointer
+	// Move the tail
+	for i := tailLength; i > 0; i-- {
+		snake[i].x = snake[i-1].x
+		snake[i].y = snake[i-1].y
 	}
-	if position.x < -buffer {
-		position.x = xMax + buffer
+
+	// Move the head
+	switch direction {
+	case up:
+		snake[0].y--
+	case left:
+		snake[0].x--
+	case down:
+		snake[0].y++
+	case right:
+		snake[0].x++
 	}
-	if position.y < -buffer {
-		position.y = yMax + buffer
+}
+
+func checkCollision(client *Client) {
+	headX := client.snake[0].x
+	headY := client.snake[0].y
+
+	for i := 0; i < len(clients); i++ {
+		if clients[i].alive == false {
+			continue
+		}
+		snakeToCheck := &clients[i].snake
+		for j := 1; j < clients[i].tailLength; j++ {
+			if headX == (*snakeToCheck)[j].x && headY == (*snakeToCheck)[j].y {
+				client.alive = false
+				println("collision")
+			}
+		}
 	}
-	if position.y > yMax+buffer {
-		position.y = -buffer
+}
+
+func wrapAround(position *Client, xMax int, yMax int, buffer int) {
+	if position.snake[0].x > xMax+buffer {
+		position.snake[0].x = -buffer
+	}
+	if position.snake[0].x < -buffer {
+		position.snake[0].x = xMax + buffer
+	}
+	if position.snake[0].y < -buffer {
+		position.snake[0].y = yMax + buffer
+	}
+	if position.snake[0].y > yMax+buffer {
+		position.snake[0].y = -buffer
 	}
 }
 
